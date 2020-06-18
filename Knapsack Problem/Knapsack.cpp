77ab20850387c3dbf91dda2c115a1d868191b1f0
raman9514 <raman9514@gmail.com>
2020-06-18 17:31:49 +0530

    knapsack greedyforweight and greedyforboth method added

--- Knapsack Problem/Knapsack.cpp
+++ Knapsack Problem/Knapsack.cpp
@@ -4,21 +4,25 @@
 #include<stdlib.h>
 using namespace std;
 
-int object[]={1,2,3};
-int profit[]={25,24,15};
-int weight[]={18,15,10};
-int bag=20;
 int n=3;
+float object[]={1,2,3};
+float profit[]={25,24,15};
+float weight[]={18,15,10};
+float *pp=new float[n];
+int bag=20;
 
-void sort(int * a1,int *a2,int * a3)
+
+
+
+void sort(float a1[],float a2[],float a3[])
 {
    
   for(int i=0;i<n;i++)
      {for(int j=0;j<n-1;j++)
            {
-               if(a1[j]<a1[j+1])
+               if(a1[j]>a1[j+1])
                 {
-                    int temp=a1[j];
+                    float temp=a1[j];
                     a1[j]=a1[j+1];
                     a1[j+1]=temp;
 
@@ -26,16 +30,16 @@ void sort(int * a1,int *a2,int * a3)
                     a2[j]=a2[j+1];
                     a2[j+1]=temp;
 
-                    temp=a2[j];
-                    a2[j]=a2[j+1];
-                    a2[j+1]=temp;
+                    temp=a3[j];
+                    a3[j]=a3[j+1];
+                    a3[j+1]=temp;
                 }
             
            }
      }
 
-}
 
+}
 
 
 void GreedyForProfit()
@@ -44,16 +48,17 @@ void GreedyForProfit()
  int mybag=0;
  
 sort(profit,object ,weight);
+
 float myprofit=0;
-for(int i=0;i<n;i++)
+for(int i=n-1;i>=0;i--)
  {if(weight[i]<=bag-mybag)
   {
     mybag+=weight[i];
     myprofit=profit[i];
-    cout<<myprofit <<" + ";
+    
   }
   else
-  { cout<<"Else block ";
+  { 
     float av = bag - mybag;
     if(av==0){break;}
     float myval = (av/weight[i])*profit[i];
@@ -65,17 +70,95 @@ for(int i=0;i<n;i++)
   }
  }
 
- cout<<"The profit is "<<endl; 
-cout<<endl<<myprofit;
+ cout<<"Greedy For Profit "<<endl; 
+cout<<"profit = "<<myprofit;
+cout<<endl;
 }
+
+
+void GreedyForWeight()
+{
+    sort(weight,profit,object);	
+	float mybag=0,myprofit=0;
+	
+	for(int i=0;i<n;i++)
+	{
+		if(weight[i]<=bag-mybag)
+		{
+			mybag+=weight[i];
+			myprofit+=profit[i];
+			
+		}
+		else{
+			float av=bag-mybag;
+			if(av<=0){break;
+			}
+			
+			float x= av / weight[i];
+			myprofit+=(x*profit[i]);
+			mybag+=av;
+			break;
+		}
+		
+	}
+	cout<<"Greedy For Weight "<<endl;
+	
+	cout<<"Profit = "<<myprofit;
+	
+
+cout<<endl;
+
+}
+
+
+GreedyForBoth()
+{ 
+ for(int i=0;i<n;i++)
+  {
+  	pp[i]=profit[i]/weight[i];
+  }
+ 
+ sort(pp,weight,profit);
+ 
+  int mybag=0;
+ 
+float myprofit=0;
+for(int i=n-1;i>=0;i--)
+ {if(weight[i]<=bag-mybag)
+  {
+    mybag+=weight[i];
+    myprofit=profit[i];
+    
+  }
+  else
+  { 
+    float av = bag - mybag;
+    if(av==0){break;}
+    float myval = (av/weight[i])*profit[i];
+    myprofit+=myval;
+    mybag+=av;
+
+    break;
+
+  }
+ }
+
+ cout<<"Greedy For Both "<<endl; 
+cout<<"profit = "<<myprofit;
+cout<<endl;
+   		
+	
+}
+
+
 
 int main()
 {
   system("cls");  
 
+
 GreedyForProfit();
-
-
-
+GreedyForWeight();
+GreedyForBoth();
 
 return 0;}
